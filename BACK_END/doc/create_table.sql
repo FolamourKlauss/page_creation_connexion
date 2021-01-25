f90872fda226c20cf69bf139fef8ff2c4b68e8fb
Mehdi Godonou <mehdi.godonou@gmail.com>
2021-01-25 18:57:04 +0100

    adding sql file for table and data

--- BACK_END/doc/create_table.sql
+++ BACK_END/doc/create_table.sql
@@ -19,6 +19,7 @@ CREATE TABLE "user" (
   "firstname" TEXT NOT NULL DEFAULT '',
   "password" TEXT NOT NULL DEFAULT '',
   "email" TEXT NOT NULL DEFAULT '',
+  "admin" BOOLEAN NOT NULL DEFAULT 'false',
   -- pour avoir la date et l'heure on utilise le type "timestamp", et pour être le plus précis possible on utilisera plutôt le type "timestampz" qui contient en plus de la date et de l'heure le fuseau horaire défini dans les locales du serveur
   -- pensez à la réunion Meet avec les collègues internationaux
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
@@ -26,42 +27,7 @@ CREATE TABLE "user" (
 );
 
 
-/* 2ème table : Card */
 
-CREATE TABLE "card" (
-  "id" SERIAL PRIMARY KEY,
-  "content" TEXT NOT NULL DEFAULT '',
-  "color" TEXT NOT NULL DEFAULT '#FFF', -- la couleur optionnelle est ici représentée par une valeur par défaut, si je ne donne pas de couleur, le fond sera blanc
-  -- si l'on veut pouvoir supprimer une liste qui contient des cartes, on est obligé de rajouter "ON DELETE CASCADE" qui aura pour conséquence de supprimer toutes les cartes qui font référence à la liste
-  "list_id" INTEGER NOT NULL REFERENCES list("id") ON DELETE CASCADE,
-  "position" INTEGER NOT NULL DEFAULT 0,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  "updated_at" TIMESTAMPTZ
-);
-
-/* 3ème table : Tag */
-
-CREATE TABLE "tag" (
-  "id" SERIAL PRIMARY KEY,
-  "name" TEXT NOT NULL DEFAULT '',
-  "color" TEXT NOT NULL DEFAULT '#FFF' ,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  "updated_at" TIMESTAMPTZ
-);
-
-/* On oublie pas la table de liaison ! */
-
--- même pour une table de liaison, utilisez systématiquement un id serial pk
-CREATE TABLE "card_has_tag" (
-  "id" SERIAL PRIMARY KEY,
-  -- si l'on veut pouvoir supprimer une carte ou un tag, on est obligé de rajouter "ON DELETE CASCADE" qui aura pour conséquence de supprimer les associations qui font référence a la carte ou le tag supprimé.
-  "card_id" INTEGER NOT NULL REFERENCES card("id") ON DELETE CASCADE,
-  "tag_id" INTEGER NOT NULL REFERENCES tag("id") ON DELETE CASCADE,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
-  -- ici pas d'updated_at car une relation ne se met pas à jour, soit on l'ajoute soit on la supprime
-);
-
--- pour card_has_tag, on ne fera pas d'UPDATE, que des DELETE pour supprimer une relation card<>tag ou des INSERT pour en ajouter
 
 /* une fois les tables crées, on va les remplir */
 
@@ -71,19 +37,15 @@ CREATE TABLE "card_has_tag" (
 -- les tables existantes sont droppées avant d'être recréées
 -- toutes les instructions dans la même transaction donc tout passe ou rien ne passe
 
-INSERT INTO "list" ("name")
-VALUES ('Première liste' );
+INSERT INTO "user" ("name", "password", "email")
+VALUES ('administrateur','123', 'a@gmail.com' );
 
-INSERT INTO "card" ("content", "color", "list_id")
-VALUES ('Carte 1', 1), -- id 1
-       ('2ème carte', '#c1e7ff', 1); -- id 2
+INSERT INTO "user" ("name", "firstname", "password", "email")
+VALUES ('leponge', 'Bob', '123', 'boblepong@gmale.coum' );
 
-INSERT INTO "tag" ("name", "color")
-VALUES ('Urgent', '#F00'); -- id 1
 
--- et on oublie pas la table de liaison !
-INSERT INTO "card_has_tag" ("card_id", "tag_id")
-VALUES (1,1); -- Carte 1, Urgent
+
+
 
 COMMIT;
 
